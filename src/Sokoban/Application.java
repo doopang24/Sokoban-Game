222989949d96ab0ed86e93fb0c9a60b88564977e
wodus4145@naver.com <wodus4145@naver.com>
2024-11-25 14:24:42 +0900

    feat: Application 클래스의 main() 메서드 추가

--- src/Sokoban/Application.java
+++ src/Sokoban/Application.java
@@ -1,4 +1,11 @@
 package Sokoban;
 
 public class Application {
+    public static void main(String[] args) {
+
+        StageReader reader = new StageReader();
+        String stage = reader.receiveStage();
+        MapInfo mapInfo = reader.parseStage(stage);
+        reader.printStage(mapInfo);
+    }
 }

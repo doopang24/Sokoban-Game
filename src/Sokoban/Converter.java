f4705e2a29b7905c21e0f969136f221aaa380050
wodus4145@naver.com <wodus4145@naver.com>
2024-11-25 21:29:21 +0900

    refactor: 숫자값을 다시 기호로 만드는 기능 추가

--- src/Sokoban/Converter.java
+++ src/Sokoban/Converter.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 public class Converter {
 
     private static final Map<Character, Integer> symbolToValue = new HashMap<>();
+    private static final Map<Integer, Character> valueToSymbol = new HashMap<>();
 
     static {
         symbolToValue.put(' ', 0);
@@ -13,9 +14,19 @@ public class Converter {
         symbolToValue.put('o', 2);
         symbolToValue.put('P', 3);
         symbolToValue.put('#', 4);
+
+        valueToSymbol.put(0, ' ');
+        valueToSymbol.put(1, 'O');
+        valueToSymbol.put(2, 'o');
+        valueToSymbol.put(3, 'P');
+        valueToSymbol.put(4, '#');
     }
 
     public int charToValue(char symbol) {
         return symbolToValue.getOrDefault(symbol, -1);
+    }
+
+    public int valueToChar(int value) {
+        return valueToSymbol.getOrDefault(value, '?');
     }
 }

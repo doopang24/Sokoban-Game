824cc3d4308ccdf0e740362147063b08ebdd9b45
wodus4145@naver.com <wodus4145@naver.com>
2024-11-25 15:43:45 +0900

    refactor: Application 클래스 수정

--- src/Sokoban/Application.java
+++ src/Sokoban/Application.java
@@ -1,11 +1,17 @@
 package Sokoban;
 
-public class Application {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Application {
+
+    public static void main(String[] args) {
         StageReader reader = new StageReader();
+        PlayerCommand command = new PlayerCommand();
+
         String stage = reader.receiveStage();
         MapInfo mapInfo = reader.parseStage(stage);
         reader.printStage(mapInfo);
+
+        char[] commandList = command.getInputCommand();
     }
 }
